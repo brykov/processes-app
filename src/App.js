518e022223466e267f5890a567ac3649b41914f8
Ivan Brykov <ivan.ru@gmail.com>
2018-06-01 00:30:16 -0500

    styles update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import s from './App.css';
 import ListContainer from './containers/ListContainer'
 
 class App extends Component {
-  render() {
-    return (
-      <div className={s.App}>
-        <header className={s["App-header"]}>
-          <h1 className={s["App-title"]}>Welcome to React</h1>
-        </header>
-        <ListContainer/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className={s.App}>
+                <header className={s.AppHeader}>
+                    <h1 className={s.AppTitle}>Welcome to React</h1>
+                </header>
+                <ListContainer/>
+            </div>
+        );
+    }
 }
 
 export default App;
